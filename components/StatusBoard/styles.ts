b33ddee1a30fcfa5f9079a1bcee60b21382e5cd3
Kielan <ki.lemons@gmail.com>
2024-03-26 14:20:11 -0700

    Create styles.ts

---
+++ components/StatusBoard/styles.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  PrimaryColor: '#D5D5DC',
+  SecondaryColor: '#FAFAFA',
+  TertiaryColor: '#E2E2EA',
+	InfoColor: '#0062FF',
+	WhatColor: '#0062FF',
+	What2Color: '#92929D',
+  ScoreColor: '#',
+  WarningColor: '#',
+	WhiteColor: '#FFFFFF'
+}
